1430a25485ff385577cd6201e7a395f81918a2cd
chuxin <15115359055@139.com>
2024-08-21 10:31:01 +0800

    feat: 删除一些无关紧要的目录

--- demo-jsx/README.md
+++
@@ -1,6 +0,0 @@
-# demo-jsx
-
-1. 下载 vite jsx 插件
-```shell
-pnpm install -D @vitejs/plugin-vue-jsx
-```
--- demo-jsx/src/main.ts
+++
@@ -1,4 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.tsx";
-
-createApp(App).mount("#app");
